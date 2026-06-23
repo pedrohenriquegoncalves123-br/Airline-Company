e3600ce94678701b7229bdcf004aed655e9da33a
pedrohenriquegoncalves123-br <pedrohenriquegoncalves564@gmail.com>
2026-06-23 17:53:55 -0300

    Add files via upload

---
+++ js/interacao.js
@@ -0,0 +1,11 @@
+
+/* window.addEventListener('scroll', () => {
+    const menu = document.querySelector('.menu');
+
+    if (window.scrollY > 50) {
+        menu.classList.add('menu-rolagem');
+    } else {
+        menu.classList.remove('menu-rolagem');
+    }
+});
+ */
